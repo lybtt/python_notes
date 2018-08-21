b50ac0b588259cdde27c2c6ddecb20ad268a1191
lybtt <812569791@qq.com>
2018-08-21 12:53:37 +0800

    unicode character

--- Unicode_character.py
+++ Unicode_character.py
@@ -11,3 +11,16 @@ print(unicodedata.name(u']'))
 print(unicodedata.category(u'A'))
 print(unicodedata.category(u']'))
 print(unicodedata.name(u'a'))
+
+
+# ord chr
+
+print(ord('A'))   # 返回一个字符串的Unicode编码
+print(chr(65))
+
+# A-Z
+str_a_z = [chr(i+ord("A")) for i in range(26)]
+print(str_a_z)
+# A-Z 与索引值结合
+dict_a_z = dict(zip(str_a_z, range(1, 27)))
+print(dict_a_z)
